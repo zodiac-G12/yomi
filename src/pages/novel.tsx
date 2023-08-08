ff7b26cf6ffc8f629393a4024be7e66f9c0375ee
zodiac-G12 <12181838+zodiac-G12@users.noreply.github.com>
2023-08-09 03:18:05 +0900

    fix: ちょいいいかんじにした

--- src/pages/novel.tsx
+++ src/pages/novel.tsx
@@ -1,14 +1,25 @@
-import {lazy, createEffect, createSignal, For, Accessor, createResource, onCleanup} from 'solid-js';
-import type { Component } from 'solid-js';
+import {
+  lazy,
+  createEffect,
+  createSignal,
+  For,
+  Accessor,
+  createResource,
+  onCleanup,
+} from 'solid-js';
+import type {Component} from 'solid-js';
 import {Router, Routes, Route, Link, useParams} from '@solidjs/router';
-import {authors, Author, Novel} from "../authors";
+import {authors, Author, Novel} from '../authors';
 import {css} from 'solid-styled-components';
 import SolidMarkdown from 'solid-markdown';
 
-const getNovel = async (props: {authorID: string, novelID: string}) => {
-  const { authorID, novelID } = props;
+const getNovel = async (props: { authorID: string; novelID: string }) => {
+  const {authorID, novelID} = props;
 
-  const path = new URL(`/src/assets/cards/${authorID}/files/${novelID}/${novelID}.txt`, import.meta.url).href;
+  const path = new URL(
+      `/src/assets/cards/${authorID}/files/${novelID}/${novelID}.txt`,
+      import.meta.url
+  ).href;
 
   const response = await fetch(path);
 
@@ -16,16 +27,23 @@ const getNovel = async (props: {authorID: string, novelID: string}) => {
 
   // const head = result.split("\n").slice(0, 3).join("\n");
 
-const inputString = "これは（置換したい）テキストです。\n改行も含まれます。------------------こちらも置換したい------------------";
-const pattern = /-{18}([\s\S]*?)-{18}/g;
-const replacedString = inputString.replace(pattern, "");
-console.log(replacedString);
-
-  const sentence = result.replaceAll(/［[^\］]*］/g,"").replaceAll(/-------------------------------------------------------([\s\S]*?)-------------------------------------------------------/g,"");;
+  const sentence = result
+      .replace(
+          /.*?(?=\-------------------------------------------------------)/s,
+          ''
+      )
+      .replaceAll(/［[^\］]*］/g, '')
+      .replaceAll(
+          /-------------------------------------------------------([\s\S]*?)-------------------------------------------------------/g,
+          ''
+      )
+      .replace(/\n{2,}/g, '\n')
+      .replace(/ +/g, ' ')
+      .replace(/—/g, '-');
 
   // return head + sentence;
   return sentence;
-}
+};
 
 const NovelDetail: Component<{
   isSP: Accessor<boolean>;
@@ -40,7 +58,7 @@ const NovelDetail: Component<{
     return <>Author Not Found</>;
   }
 
-  const { novels } = author;
+  const {novels} = author;
 
   if (!novels) {
     return <>Novels Not Found</>;
@@ -52,27 +70,40 @@ const NovelDetail: Component<{
     return <>Novel Not Found</>;
   }
 
-  const [data] = createResource({ authorID: params.authorID, novelID: params.novelID }, getNovel);
+  const [data] = createResource(
+      {authorID: params.authorID, novelID: params.novelID},
+      getNovel
+  );
 
   const [sentence, setSentence] = createSignal(data()?.toString());
 
+  const [isStop, setIsStop] = createSignal(false);
+
+  const handleClick = () => {
+    setIsStop(!isStop());
+  };
+
   createEffect(async () => {
     const dataValue = data();
+
+    if (isStop()) {
+      return;
+    }
 
     if (!dataValue) {
       const timer = setInterval(() => setCount(count() + 0), 100);
       onCleanup(() => clearInterval(timer));
     } else {
-      const timer = setInterval(() => (
-        setCount(count() + 1),
-        setSentence(dataValue.slice(0, count()))
-      ), 100);
+      const timer = setInterval(
+          () => (setCount(count() + 1), setSentence(dataValue.slice(0, count()))),
+          100
+      );
       onCleanup(() => clearInterval(timer));
     }
-  })
+  });
 
   return (
-    <div class={NovelDetailContainer}>
+    <div class={NovelDetailContainer} onClick={handleClick}>
       <div class={NovelTitle}>{novel.novelTitle}</div>
       <div class={NovelAuthor}>{novel.author}</div>
       <div class={NovelSentence}>{sentence()}</div>
@@ -83,34 +114,42 @@ const NovelDetail: Component<{
       */}
     </div>
   );
-}
+};
 
 export default NovelDetail;
 
 const NovelDetailContainer = css({
   background: 'black',
-  padding: "10px",
+  padding: '10px',
+  position: 'fixed',
+  height: 'calc(100vh - 20px)',
+  width: 'calc(100vw - 20px)',
+  zIndex: 1,
 });
 
 const NovelTitle = css({
-  color: "white",
-  textAlign: "center",
+  color: 'white',
+  textAlign: 'center',
   fontSize: '25px',
-  fontFamily: "MyFont",
-  padding: "10px",
+  fontFamily: 'MyFont',
+  padding: '10px',
+  zIndex: 2,
 });
 
 const NovelAuthor = css({
-  color: "white",
-  textAlign: "center",
+  color: 'white',
+  textAlign: 'center',
   fontSize: '20px',
-  fontFamily: "MyFont",
-  padding: "10px",
-  marginBottom: "20px",
+  fontFamily: 'MyFont',
+  padding: '10px',
+  marginBottom: '20px',
+  zIndex: 2,
 });
 
 const NovelSentence = css({
-  color: "white",
+  color: 'white',
   fontSize: '18px',
-  fontFamily: "MyFont",
+  fontFamily: 'MyFont',
+  whiteSpace: 'pre-line',
+  zIndex: 2,
 });
