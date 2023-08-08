17709c58d225627bc57521f2aa178edfe0de32ec
zodiac-G12 <12181838+zodiac-G12@users.noreply.github.com>
2023-08-09 02:16:35 +0900

    feat: -の削除の正規表現追加

--- src/pages/novel.tsx
+++ src/pages/novel.tsx
@@ -1,22 +1,14 @@
-import {
-  createEffect,
-  createSignal,
-  Accessor,
-  createResource,
-  onCleanup,
-} from 'solid-js';
-import type {Component} from 'solid-js';
-import {useParams} from '@solidjs/router';
-import {authors} from '../authors';
+import {lazy, createEffect, createSignal, For, Accessor, createResource, onCleanup} from 'solid-js';
+import type { Component } from 'solid-js';
+import {Router, Routes, Route, Link, useParams} from '@solidjs/router';
+import {authors, Author, Novel} from "../authors";
 import {css} from 'solid-styled-components';
+import SolidMarkdown from 'solid-markdown';
 
-const getNovel = async (props: { authorID: string; novelID: string }) => {
-  const {authorID, novelID} = props;
+const getNovel = async (props: {authorID: string, novelID: string}) => {
+  const { authorID, novelID } = props;
 
-  const path = new URL(
-      `/src/assets/cards/${authorID}/files/${novelID}/${novelID}.txt`,
-      import.meta.url
-  ).href;
+  const path = new URL(`/src/assets/cards/${authorID}/files/${novelID}/${novelID}.txt`, import.meta.url).href;
 
   const response = await fetch(path);
 
@@ -24,19 +16,20 @@ const getNovel = async (props: { authorID: string; novelID: string }) => {
 
   // const head = result.split("\n").slice(0, 3).join("\n");
 
-  const sentence = result
-      .split('\n')
-      .slice(18)
-      .join('\n')
-      .replaceAll(/［[^\］]*］/g, '');
+const inputString = "これは（置換したい）テキストです。\n改行も含まれます。------------------こちらも置換したい------------------";
+const pattern = /-{18}([\s\S]*?)-{18}/g;
+const replacedString = inputString.replace(pattern, "");
+console.log(replacedString);
+
+  const sentence = result.replaceAll(/［[^\］]*］/g,"").replaceAll(/-------------------------------------------------------([\s\S]*?)-------------------------------------------------------/g,"");;
 
   // return head + sentence;
   return sentence;
-};
+}
 
 const NovelDetail: Component<{
   isSP: Accessor<boolean>;
-}> = () => {
+}> = (props) => {
   const params = useParams();
 
   const [count, setCount] = createSignal(0);
@@ -47,7 +40,7 @@ const NovelDetail: Component<{
     return <>Author Not Found</>;
   }
 
-  const {novels} = author;
+  const { novels } = author;
 
   if (!novels) {
     return <>Novels Not Found</>;
@@ -59,10 +52,7 @@ const NovelDetail: Component<{
     return <>Novel Not Found</>;
   }
 
-  const [data] = createResource(
-      {authorID: params.authorID, novelID: params.novelID},
-      getNovel
-  );
+  const [data] = createResource({ authorID: params.authorID, novelID: params.novelID }, getNovel);
 
   const [sentence, setSentence] = createSignal(data()?.toString());
 
@@ -73,49 +63,54 @@ const NovelDetail: Component<{
       const timer = setInterval(() => setCount(count() + 0), 100);
       onCleanup(() => clearInterval(timer));
     } else {
-      const timer = setInterval(
-          () => (setCount(count() + 1), setSentence(dataValue.slice(0, count()))),
-          100
-      );
+      const timer = setInterval(() => (
+        setCount(count() + 1),
+        setSentence(dataValue.slice(0, count()))
+      ), 100);
       onCleanup(() => clearInterval(timer));
     }
-  });
+  })
 
   return (
     <div class={NovelDetailContainer}>
       <div class={NovelTitle}>{novel.novelTitle}</div>
       <div class={NovelAuthor}>{novel.author}</div>
       <div class={NovelSentence}>{sentence()}</div>
+      {/* <SolidMarkdown
+        class={NovelSentence}
+        children={sentence()}
+      />
+      */}
     </div>
   );
-};
+}
 
 export default NovelDetail;
 
 const NovelDetailContainer = css({
   background: 'black',
-  padding: '10px',
+  padding: "10px",
 });
 
 const NovelTitle = css({
-  color: 'white',
-  textAlign: 'center',
+  color: "white",
+  textAlign: "center",
   fontSize: '25px',
-  fontFamily: 'MyFont',
-  padding: '10px',
+  fontFamily: "MyFont",
+  padding: "10px",
 });
 
 const NovelAuthor = css({
-  color: 'white',
-  textAlign: 'center',
+  color: "white",
+  textAlign: "center",
   fontSize: '20px',
-  fontFamily: 'MyFont',
-  padding: '10px',
-  marginBottom: '20px',
+  fontFamily: "MyFont",
+  padding: "10px",
+  marginBottom: "20px",
 });
 
 const NovelSentence = css({
-  color: 'white',
+  color: "white",
   fontSize: '18px',
-  fontFamily: 'MyFont',
+  fontFamily: "MyFont",
 });
